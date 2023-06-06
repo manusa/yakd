2452784968a331a837faa319f475d7cf06e65cdc
Marc Nuri <marc@marcnuri.com>
2023-06-06 11:41:32 +0200

    deps: bump react to 18.2.0

--- src/main/frontend/src/index.js
+++ src/main/frontend/src/index.js
@@ -15,7 +15,7 @@
  *
  */
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {createRoot} from 'react-dom/client';
 import {Provider} from 'react-redux';
 import {combineReducers, createStore} from 'redux';
 import apis from './apis';
@@ -96,11 +96,11 @@ const store = createStore(
   storeEnhancer()
 );
 
-ReactDOM.render(
+const root = createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
