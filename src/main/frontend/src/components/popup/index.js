7099e023e3e77d344ce311251e54c7edf2b5918e
Marc Nuri <marc@marcnuri.com>
2025-12-15 07:04:29 +0100

    chore(style): standardize copyright year to 2020 in license headers

--- src/main/frontend/src/components/popup/index.js
+++ src/main/frontend/src/components/popup/index.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 Marc Nuri
+ * Copyright 2020 Marc Nuri
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
